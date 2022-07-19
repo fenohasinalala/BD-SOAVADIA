7574d43a0af79360cb105b1d58feb27b2de59e02
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2022-07-19 08:15:15 +0300

    ajout de quelque resultat

--- LISTES_REQUETES/les_details_des_reservations_qui_ne_sont_pas_encore_payees.sql
+++ LISTES_REQUETES/les_details_des_reservations_qui_ne_sont_pas_encore_payees.sql
@@ -17,3 +17,41 @@ inner join ville villeD on trajet.id_ville_depart = villeD.id_ville
 inner join ville villeA on trajet.id_ville_arrivee = villeA.id_ville
 
 where payement.payement_complet = false;
+
+id_reservation | date_reservation | numero_place |       nom       |          prenom           | date_depart | horaire_depart | ville_depart | ville_arrivee | id_recep
+tionniste | payement_complet 
+----------------+------------------+--------------+-----------------+---------------------------+-------------+----------------+--------------+---------------+---------
+----------+------------------
+             23 | 2022-07-02       |            7 | Raharison       | Marcel                    | 2022-07-11  | 12:00:00       | Mahajanga    | Antsiranana   |         
+       14 | f
+             49 | 2022-07-04       |            5 | Luc             | Odon                      | 2022-07-11  | 07:00:00       | Mahajanga    | Antananarivo  |         
+       10 | f
+             52 | 2022-07-06       |            3 | Haja            | Anja                      | 2022-07-11  | 12:00:00       | Antsirabe    | Fianarantsoa  |         
+       12 | f
+             97 | 2022-06-23       |            6 | Frederic        | Henri                     | 2022-07-11  | 12:00:00       | Antananarivo | Mahajanga     |         
+        8 | f
+            117 | 2022-06-28       |           11 | Rajaonarivelo   | Alphonse                  | 2022-07-11  | 18:00:00       | Antananarivo | Fianarantsoa  |         
+       32 | f
+            136 | 2022-07-01       |            3 | Gilbert         | Sandra                    | 2022-07-11  | 12:00:00       | Antsirabe    | Antananarivo  |         
+       14 | f
+            163 | 2022-06-14       |           13 | Andrianirina    | Eddy                      | 2022-07-12  | 07:00:00       | Antsiranana  | Mahajanga     |         
+       13 | f
+            185 | 2022-07-07       |            6 | William         | Julie                     | 2022-07-12  | 18:00:00       | Antananarivo | Antsirabe     |         
+       32 | f
+            234 | 2022-06-11       |            5 | Salava          | Harry                     | 2022-07-12  | 07:00:00       | Antsiranana  | Mahajanga     |         
+       27 | f
+            295 | 2022-07-02       |            2 | Leon            | Laurent                   | 2022-07-13  | 07:00:00       | Mahajanga    | Antananarivo  |         
+       14 | f
+            308 | 2022-07-08       |            4 | Rajonson        | William                   | 2022-07-13  | 12:00:00       | Antananarivo | Antsirabe     |         
+       21 | f
+            328 | 2022-06-17       |           11 | Rakotoarisoa    | Francine                  | 2022-07-13  | 18:00:00       | Mahajanga    | Antananarivo  |         
+       26 | f
+            334 | 2022-06-16       |            3 | Fock            | Bertrand                  | 2022-07-13  | 07:00:00       | Antananarivo | Antsirabe     |         
+        3 | f
+            338 | 2022-06-11       |            2 | Ra              | Yves                      | 2022-07-13  | 12:00:00       | Antananarivo | Mahajanga     |         
+       12 | f
+            375 | 2022-07-01       |           10 | Rasoanirina     | Ferdinand-Laurence        | 2022-07-13  | 12:00:00       | Antananarivo | Toamasina     |         
+       11 | f
+            366 | 2022-07-04       |            1 | Zo              | Eliane                    | 2022-07-13  | 12:00:00       | Antananarivo | Toamasina     |         
+       28 | f
+            429 | 2022-07-06       |            9 | James           | Estelle                   | 2022-07-13  | 18:00:00       | Antananarivo | Fianarantsoa  |  
--- LISTES_REQUETES/liste_client_selon_offre.sql
+++ LISTES_REQUETES/liste_client_selon_offre.sql
@@ -9,3 +9,28 @@ inner join voyage on voyage.id_voyage = reservation.id_voyage
 inner join voiture on voyage.id_voiture = voiture.id_voiture
 inner join offre on voiture.id_offre = offre.id_offre
 where offre.id_offre = 3;
+
+
+id_reservation |       nom       |          prenom           | label_offre 
+----------------+-----------------+---------------------------+-------------
+             45 | Marius          | Rosette                   | vip
+             46 | Ismael          | Olga                      | vip
+             47 | Nelson          | Stanislas-Maxime          | vip
+             48 | Fano            | Yolande                   | vip
+             49 | Luc             | Odon                      | vip
+            146 | Bezara          | Lydia                     | vip
+            147 | Soja            | Anja                      | vip
+            148 | Roland          | Fabien                    | vip
+            149 | James           | Jimmy                     | vip
+            150 | Johnson         | Stella                    | vip
+            244 | Be              | Benjamin                  | vip
+            245 | Charles         | Frederic                  | vip
+            246 | Randrianasolo   | Sarah                     | vip
+            247 | Andrianjanaka   | Annick                    | vip
+            262 | Zo              | Noel                      | vip
+            263 | Andrian         | Gerard                    | vip
+            264 | Rajaobelina     | Lila                      | vip
+            265 | Rasoanirina     | Ferdinand-Laurence        | vip
+            266 | Rakotondrazaka  | Julienne                  | vip
+            267 | Rand            | Lucie                     | vip
+            332 | Jonah           | Celestin                  | vip
